3931facea7507151a53f209ca4f8a717fa6bd400
cormoran <cormoran707@gmail.com>
2025-05-03 12:33:51 +0900

    Enable to configure battery voltage threshold

--- app/module/drivers/sensor/battery/battery_common.c
+++ app/module/drivers/sensor/battery/battery_common.c
@@ -41,3 +41,28 @@ uint8_t lithium_ion_mv_to_pct(int16_t bat_mv) {
 
     return bat_mv * 2 / 15 - 459;
 }
+
+uint8_t mv_to_pct_linear_interpolation(int16_t bat_mv, int16_t *mv_thresholds,
+                                       size_t mv_thresholds_size) {
+    if (bat_mv < mv_thresholds[0]) {
+        return 0;
+    }
+    if (bat_mv >= mv_thresholds[mv_thresholds_size - 1]) {
+
+        return 100;
+    }
+    for (size_t i = 1; i < mv_thresholds_size; i++) {
+        if (bat_mv < mv_thresholds[i]) {
+            int low = mv_thresholds[i - 1];
+            int high = mv_thresholds[i];
+            // [i-1] -> bat_mv -> [i]
+            //  A: [0  ]->[i-1 ] = i-1 units = (i-1) / (size - 1)
+            //  B: [i-1]->bat_mv = 1 unit * (bat_mv - low) / (high - low)
+            //                   = 1 / (size - 1) * (bat_mv - low) / (high - low)
+            // percentage = (A + B) * 100
+            float deno = ((bat_mv - low) + (i - 1) * (high - low)) * 100;
+            return deno / (float)((high - low) * (mv_thresholds_size - 1));
+        }
+    }
+    return 100;
+}
